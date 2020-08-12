7af40dfc7c82d20cfeea7c43e36c44631faa2046
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2020-08-12 21:10:59 +0530

    genesis

---
+++ src/main/java/com/isuru/docx/DocxApplication.java
@@ -0,0 +1,13 @@
+package com.isuru.docx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isuru/docx/DocxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isuru.docx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
